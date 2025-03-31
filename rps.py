39491d532fb8dad6ec7cb099e7f8dd558eb4c465
luca <lucascarpantonio@MacLuca.local>
2025-03-31 14:02:45 +0200

    Code formatted according to pycodestyle.

--- rps.py
+++ rps.py
@@ -2,10 +2,11 @@ import random
 
 moves = ['rock', 'paper', 'scissors']
 
+
 class Player:
     def __init__(self):
         self.score = 0
-    
+
     def move(self):
         return 'rock'
 
@@ -15,17 +16,26 @@ class Player:
 
 # Something similar to a truth table is needed to determine the winner.
 # Rreturns True if a wins against be, otherwise False.
+
+
 def who_wins(one, two):
+
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
             (one == 'paper' and two == 'rock'))
 
 # Here a Class to introduce the RealPlayer, it allows interaction.
-# If 'quit' is typed as move, the program ends. This is needed to exit before the number of turn is reached.
+# If 'quit' is typed as move, the program ends.
+# This is needed to exit before the number of turn is reached.
+
+
 class RealPlayer(Player):
+
     def move(self):
         while True:
-            move = input("Inserisci la tua mossa (rock, paper, scissors o 'quit' per uscire): ").strip().lower()
+            move = input("Inserisci la tua mossa\
+                        (rock, paper, scissors o\
+                        'quit' per uscire): ").strip().lower()
             if move == 'quit':
                 print("Hai scelto di uscire dal gioco.")
                 exit()
@@ -33,13 +43,20 @@ class RealPlayer(Player):
                 return move
             print("Mossa non valida. Riprova.")
 
-# RandomPlayer is the PC which returns something chosen randomly from the moves list.
+# RandomPlayer is the PC which returns
+# something chosen randomly from the moves list.
+
+
 class RandomPlayer(Player):
+
     def move(self):
         return random.choice(moves)
 
-# This Class manages the game logic. Players have a move each turn and after each turn the score gets updated.
+# This Class manages the game logic. Players have a
+# move each turn and after each turn the score gets updated.
 # A the end of the game the total score is displayed
+
+
 class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -69,10 +86,12 @@ class Game:
             print(f"Round {round}:")
             self.play_round()
         print("Game over!")
-        print(f"Final Score -> Player 1: {self.p1.score}, Player 2: {self.p2.score}")
+        print(f"Final Score -> Player 1: {self.p1.score},\
+               Player 2: {self.p2.score}")
 
 
-# Adding condition that allows to use the script directly or to import it without executing (or that's what I understood...)
+# Adding condition that allows to use the script directly
+# or to import it without executing (or that's what I understood...)
 if __name__ == '__main__':
     game = Game(RealPlayer(), RandomPlayer())
     game.play_game()
